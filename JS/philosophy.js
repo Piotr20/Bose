f9389dcdb59d789667ed88df3ecd9d1bc3eb1d9c
Piotr20 <piotrpospiech00@gmail.com>
2020-01-04 23:33:49 +0100

    about us section started

---
+++ JS/philosophy.js
@@ -0,0 +1,84 @@
+const img1 = document.querySelector('.intro .image_box img:nth-of-type(1)');
+const img2 = document.querySelector('.intro .image_box img:nth-of-type(2)');
+const img3 = document.querySelector('.intro .image_box img:nth-of-type(3)');
+const img4 = document.querySelector('.intro .image_box img:nth-of-type(4)');
+const img5 = document.querySelector('.intro .image_box img:nth-of-type(5)');
+const img6 = document.querySelector('.intro .image_box img:nth-of-type(6)');
+const img7 = document.querySelector('.intro .image_box img:nth-of-type(7)');
+const img8 = document.querySelector('.intro .image_box img:nth-of-type(8)');
+const img9 = document.querySelector('.intro .image_box img:nth-of-type(9)');
+const img10 = document.querySelector('.intro .image_box img:nth-of-type(10)');
+const img11 = document.querySelector('.intro .image_box img:nth-of-type(11)');
+const img12 = document.querySelector('.intro .image_box img:nth-of-type(12)');
+const img13 = document.querySelector('.intro .image_box img:nth-of-type(13)');
+const img14 = document.querySelector('.intro .image_box img:nth-of-type(14)');
+
+function imgadd1() {
+     img1.classList.add('active');
+}
+setTimeout(imgadd1, 8000);
+
+function imgadd2() {
+     img2.classList.add('active');
+}
+setTimeout(imgadd2, 8000);
+
+function imgadd3() {
+     img3.classList.add('active');
+}
+setTimeout(imgadd3, 8000);
+
+function imgadd4() {
+     img4.classList.add('active');
+}
+setTimeout(imgadd4, 8000);
+
+function imgadd5() {
+     img5.classList.add('active');
+}
+setTimeout(imgadd5, 8000);
+
+function imgadd6() {
+     img6.classList.add('active');
+}
+setTimeout(imgadd6, 8000);
+
+function imgadd7() {
+     img7.classList.add('active');
+}
+setTimeout(imgadd7, 8000);
+
+function imgadd8() {
+     img8.classList.add('active');
+}
+setTimeout(imgadd8, 8000);
+
+function imgadd9() {
+     img9.classList.add('active');
+}
+setTimeout(imgadd9, 8000);
+
+function imgadd10() {
+     img10.classList.add('active');
+}
+setTimeout(imgadd10, 8000);
+
+function imgadd11() {
+     img11.classList.add('active');
+}
+setTimeout(imgadd11, 8000);
+
+function imgadd12() {
+     img12.classList.add('active');
+}
+setTimeout(imgadd12, 8000);
+
+function imgadd13() {
+     img13.classList.add('active');
+}
+setTimeout(imgadd13, 8000);
+
+function imgadd14() {
+     img14.classList.add('active');
+}
+setTimeout(imgadd14, 8000);
